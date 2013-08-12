e93616c216a7f766d63a9277c7338d12bd8f37b0
orangain <orangain@gmail.com>
2013-08-12 22:44:32 +0900

    Refactor setup* methods into TwistedMessageQueue

--- worker.py
+++ worker.py
@@ -7,51 +7,70 @@ from twisted.internet import reactor, task, defer, protocol
 
 PREV_QUEUE_NAME = 'prev_queue'
 NEXT_QUEUE_NAME = 'next_queue'
+AMQP_DEFAULT_PORT = 5672
 
 logger = logging.getLogger(__name__)
 
-def setup_consumer(queue_name, callback, on_close):
+class TwistedMessageQueue(object):
+
+    def __init__(self, host, port, queue_name):
+        port = port or AMQP_DEFAULT_PORT
+
+        self.queue_name = queue_name
+        self.parameters = pika.ConnectionParameters(
+                host=host, port=port)
+        self.on_close = lambda failure: None
+        self.on_ready = lambda channel: None
+
+        self._connect()
+
+    def _connect(self):
+        cc = protocol.ClientCreator(reactor, TwistedProtocolConnection,
+                self.parameters)
+        d = retry(5, 4,
+                cc.connectTCP, self.parameters.host, self.parameters.port)
+        d.addCallback(lambda protocol: protocol.ready)
+        d.addCallback(self._on_connect)
 
     @defer.inlineCallbacks
-    def run(connection):
+    def _on_connect(self, connection):
+        logger.info('Connected to %s:%s' % (self.parameters.host, self.parameters.port))
         d = defer.Deferred()
-        d.addErrback(on_close)
+        d.addErrback(self._on_close)
         connection.ready = d
 
-        channel = yield connection.channel()
-        queue = yield channel.queue_declare(queue=queue_name)
-        yield channel.basic_qos(prefetch_count=1)
-        queue_object, consumer_tag = yield channel.basic_consume(
-                queue=queue_name, no_ack=False)
+        self.channel = yield connection.channel()
+        queue = yield self.channel.queue_declare(queue=self.queue_name)
+        yield self._on_ready(self.channel)
+
+    def _on_close(self, failure):
+        return self.on_close(failure)
+
+    def _on_ready(self, channel):
+        return self.on_ready(channel)
+
+    @defer.inlineCallbacks
+    def consume(self, callback):
+
+        @defer.inlineCallbacks
+        def read(queue_object):
+            ch, method, properties, body = yield queue_object.get()
+            yield callback(body)
+            yield ch.basic_ack(delivery_tag=method.delivery_tag)
+
+        yield self.channel.basic_qos(prefetch_count=1)
+        queue_object, consumer_tag = yield self.channel.basic_consume(
+                queue=self.queue_name, no_ack=False)
         l = task.LoopingCall(read, queue_object)
         l.start(0.01)
 
-    @defer.inlineCallbacks
-    def read(queue_object):
-        ch,method,properties,body = yield queue_object.get()
-        yield callback(body)
-        yield ch.basic_ack(delivery_tag=method.delivery_tag)
+        logger.info('Start consuming')
 
-    parameters = pika.ConnectionParameters()
-    cc = protocol.ClientCreator(reactor, TwistedProtocolConnection, parameters)
-    d = retry(5, 4, lambda: cc.connectTCP(parameters.host, parameters.port))
-    d.addCallback(lambda protocol: protocol.ready)
-    d.addCallback(run)
-
-def setup_publisher(queue_name, on_ready, on_close):
-
-    @defer.inlineCallbacks
-    def run(connection):
-        connection.add_on_close_callback(on_close)
-        channel = yield connection.channel()
-        queue = yield channel.queue_declare(queue=queue_name)
-        yield on_ready(channel)
-
-    parameters = pika.ConnectionParameters()
-    cc = protocol.ClientCreator(reactor, TwistedProtocolConnection, parameters)
-    d = retry(5, 4, lambda: cc.connectTCP(parameters.host, parameters.port))
-    d.addCallback(lambda protocol: protocol.ready)
-    d.addCallback(run)
+    def publish(self, message):
+        return self.channel.basic_publish(
+            exchange='',
+            routing_key=self.queue_name,
+            body=message)
 
 def retry(times, sleep, func, *args, **kwargs):
     """retry a defer function
@@ -86,28 +105,31 @@ def retry(times, sleep, func, *args, **kwargs):
 class Worker(object):
 
     def run(self):
-        setup_publisher(NEXT_QUEUE_NAME, self.on_ready, self.on_close)
-        setup_consumer(PREV_QUEUE_NAME, self.on_consume, self.on_close)
+        self.next_queue = TwistedMessageQueue('localhost', 5672, NEXT_QUEUE_NAME)
+        self.next_queue.on_close = self.on_close
+
+        self.prev_queue = TwistedMessageQueue('localhost', 5672, PREV_QUEUE_NAME)
+        self.prev_queue.on_close = self.on_close
+        self.prev_queue.on_ready = self.on_prev_queue_ready
 
         reactor.run()
+
+    def on_prev_queue_ready(self, channel):
+        logger.info('Prev queue is ready!')
+        return self.prev_queue.consume(self.on_consume)
 
     def on_consume(self, body):
         logger.info('Received: %s' % body)
         body = body.upper()
         logger.info('Publishing: %s' % body)
 
-        return self.next_channel.basic_publish(
-            exchange='',
-            routing_key=NEXT_QUEUE_NAME,
-            body=body)
-
-    def on_ready(self, channel):
-        logger.info('Ready!')
-        self.next_channel = channel
+        return self.next_queue.publish(body)
 
     def on_close(self, failure):
         logger.error('Connection closed: %s' % failure)
-        reactor.stop()
+        if reactor.running:
+            reactor.stop()
+
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
